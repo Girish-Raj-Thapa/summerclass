71a95d1e278ea7fcc9cbd3da55b81d523875edc0
Girish Raj Thapa <girishrajthapa03033@gmail.com>
2025-08-20 11:32:24 +0545

    dynamic setting for database for render and local machine

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -101,11 +101,25 @@ AUTH_USER_MODEL = 'accounts.Account'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
+
+if os.environ.get("RENDER"):  # Render sets this automatically
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
